d86b63144de8905d94081c35d863a69ea92cdc99
Shahil <shahilcode@gmail.com>
2024-05-24 23:54:27 +0530

    refactored the code and made the btn-common for typescript for only

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,23 +2,25 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { NgOptimizedImage } from '@angular/common';
+import { JsonPipe, NgOptimizedImage } from '@angular/common';
 import { BtnCommonComponent } from '../../shared/components/btn-common/btn-common.component';
 import { CommonBtnClass } from '../../models/auth/login';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ButtonModule, RippleModule, NgOptimizedImage, BtnCommonComponent],
+  imports: [RouterLink, ButtonModule, RippleModule, NgOptimizedImage, BtnCommonComponent,JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
 
   loginBtn = new CommonBtnClass()
+  items:any
 
   ngOnInit(): void {
     this.onLoad()
+
   }
 
   onLoad(){
@@ -30,4 +32,6 @@ this.loginBtn.BgColor='black'
     this.loginBtn.icon ='arrow_forward'
 this.loginBtn.textColor='white'
   }
+
+
 }
--- src/app/shared/components/btn-common/btn-common.component.ts
+++ src/app/shared/components/btn-common/btn-common.component.ts
@@ -8,8 +8,16 @@ import { CommonModule } from '@angular/common';
   selector: 'app-btn-common',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './btn-common.component.html',
-  styleUrl: './btn-common.component.scss',
+  template: `
+  <button class="w-full  p-4 text-2xl text-center rounded-lg btn" [ngStyle]="{
+    'color': CommonBtnData.textColor ,
+    'background-color': CommonBtnData.BgColor ,
+  }">
+  <ng-content></ng-content>
+  <span class="absolute ml-2 text-3xl material-symbols-outlined">{{CommonBtnData.icon}}</span>
+</button>
+  `,
+
 })
 export class BtnCommonComponent  {
   @Input({required:true}) CommonBtnData!: CommonBtnType
